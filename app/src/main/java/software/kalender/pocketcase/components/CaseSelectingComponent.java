82a3c9f36a6b0f07b711e84349bd4107ca34c4f0
Muhammed Kalender <muhammedkalender@protonmail.com>
2020-06-03 18:17:19 +0300

    - View Split from Test View ( Main ) - Beginning to first component ( Case Select )

--- app/src/main/java/software/kalender/pocketcase/components/CaseSelectingComponent.java
+++ app/src/main/java/software/kalender/pocketcase/components/CaseSelectingComponent.java
@@ -1,5 +1,12 @@
 package software.kalender.pocketcase.components;
 
+import android.content.Context;
+import android.view.View;
+
 public class CaseSelectingComponent {
     //TODO
+
+    public View getView(Context context){
+
+    }
 }
