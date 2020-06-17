78a4264984cbcf80af76b3ce49b5395e4b42b0bf
Muhammed Kalender <muhammedkalender@protonmail.com>
2020-06-17 17:52:40 +0300

    - Beginning to Achievement

--- app/src/main/java/software/kalender/pocketcase/models/AchievementModel.java
+++ app/src/main/java/software/kalender/pocketcase/models/AchievementModel.java
@@ -1,7 +1,16 @@
 package software.kalender.pocketcase.models;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 
 @Entity(tableName = "achievements")
 public class AchievementModel {
+    @ColumnInfo(name = "achievementId")
+    public Long achievementId;
+
+    @ColumnInfo(name = "achievementNameCode")
+    public String achievementNameCode;
+
+    @ColumnInfo(name = "achievementIcon")
+    public String achievementIcon;
 }
